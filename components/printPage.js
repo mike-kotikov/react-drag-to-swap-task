b743fb1d0acf4245b47652943f04504485c73908
mike-kotikov <kotikov.misha@gmail.com>
2022-07-13 14:27:18 +0300

    implement drag and drop functionality

--- components/printPage.js
+++ components/printPage.js
@@ -1,26 +1,27 @@
-import styled from "styled-components";
-import Actions from "./actions";
+import { useState } from 'react'
+import styled from 'styled-components'
+import Actions from './actions'
 
 const Wrapper = styled.div`
   width: 600px;
   margin: auto;
   color: #585858;
-`;
+`
 
-const PrintWrapper = styled.div``;
+const PrintWrapper = styled.div``
 
 const Header = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-`;
+`
 
 const Title = styled.div`
   font-style: normal;
   font-weight: 700;
   font-size: 16px;
   line-height: 20px;
-`;
+`
 
 const PageLayout = styled.div`
   display: flex;
@@ -30,40 +31,98 @@ const PageLayout = styled.div`
   padding: 20px;
   margin: 17px 0 42px;
   justify-content: space-between;
-`;
+`
 
 const PrintPhoto = styled.div`
-  width: calc(50% - 10px);
+  width: 270px;
+  height: 150px;
+  background-size: cover;
+  background-image: url(${props => props.bgImg});
+`
 
-  img {
-    max-width: 100%;
+const DraggableImage = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 5px solid white;
+  visibility: ${props => (props.isDragging ? 'visible' : 'hidden')};
+  user-select: none;
+  position: absolute;
+  object-fit: cover;
+  transform: translate(-50%, -50%);
+`
+
+export default function PrintPage({ data, handleDrop }) {
+  const [dragImageUrl, setDragImageUrl] = useState('')
+
+  const onMouseMove = (elem, pageX, pageY) => {
+    if (!elem) {
+      return
+    }
+    elem.style.top = `${pageY}px`
+    elem.style.left = `${pageX}px`
   }
-`;
 
-export default function PrintPage({ data }) {
+  const handleMouseUp = event => {
+    setDragImageUrl('')
+
+    setTimeout(() => {
+      const droppedToElement = document.elementFromPoint(event.clientX, event.clientY)
+
+      if (!droppedToElement || !droppedToElement.dataset.hasOwnProperty('droppable')) {
+        return
+      }
+
+      const targetImageUrl = droppedToElement.dataset.droppable
+      handleDrop(event.target.src, targetImageUrl)
+    }, 0)
+  }
+
+  const handleMouseMove = ({ pageX, pageY, target }) => {
+    onMouseMove(target, pageX, pageY)
+
+    target.addEventListener('mouseup', handleMouseUp)
+
+    target.ondragstart = () => false
+  }
+
+  const handleImageClick = ({ pageX, pageY, target }) => {
+    onMouseMove(target.firstChild, pageX, pageY)
+    setDragImageUrl(target.dataset.droppable)
+  }
+
   return (
     <>
       <Wrapper>
-        {Object.values(data).map((entry, i) => {
+        {Object.values(data).map((entry, index) => {
           return (
-            <PrintWrapper key={i}>
+            <PrintWrapper key={entry.title}>
               <Header>
                 <Title>{entry.title}</Title>
                 <Actions />
               </Header>
-              <PageLayout>
-                {entry.images.map((image) => {
+              <PageLayout data-droppable={`page-${index}`}>
+                {entry.images.map(imageUrl => {
                   return (
-                    <PrintPhoto key={image}>
-                      <img src={image} alt="" />
+                    <PrintPhoto
+                      key={imageUrl}
+                      bgImg={imageUrl}
+                      data-droppable={imageUrl}
+                      onMouseDown={handleImageClick}
+                    >
+                      <DraggableImage
+                        src={imageUrl}
+                        isDragging={dragImageUrl === imageUrl}
+                        onMouseMove={handleMouseMove}
+                      />
                     </PrintPhoto>
-                  );
+                  )
                 })}
               </PageLayout>
             </PrintWrapper>
-          );
+          )
         })}
       </Wrapper>
     </>
-  );
+  )
 }
--- pages/testpage.js
+++ pages/testpage.js
@@ -1,6 +1,7 @@
-import Head from "next/head";
-import PrintPage from "../components/printPage";
-import styled from "styled-components";
+import Head from 'next/head'
+import PrintPage from '../components/printPage'
+import styled from 'styled-components'
+import { useState } from 'react'
 
 const PageHeader = styled.div`
   width: 600px;
@@ -22,9 +23,79 @@ const PageHeader = styled.div`
     color: #797979;
     margin: 0;
   }
-`;
+`
+
+const DEFAULT_DATA = [
+  {
+    title: 'Front Print',
+    images: [
+      'https://videodelivery.net/775b1b7196b2c126b8dc343416211fdb/thumbnails/thumbnail.jpg?height=1080'
+    ]
+  },
+  {
+    title: 'Page 2',
+    images: [
+      'https://videodelivery.net/9ad2bb839e4e3cc1074e5d73b0a0379b/thumbnails/thumbnail.jpg?height=1080',
+      'https://imagedelivery.net/66_qOEcY2UwnECf5ON9PhQ/bde5b129-52ba-4f43-b3f4-97591952ea00/large'
+    ]
+  },
+  {
+    title: 'Page 3',
+    images: [
+      'https://videodelivery.net/91097538e177847ebeb934a492e146e9/thumbnails/thumbnail.jpg?height=1080',
+      'https://imagedelivery.net/66_qOEcY2UwnECf5ON9PhQ/b73c2865-7a02-408b-654d-89ce2512ae00/large'
+    ]
+  }
+]
 
 export default function Testpage() {
+  const [data, setData] = useState(DEFAULT_DATA)
+
+  const handleDrop = (draggedImageUrl, targetImageUrl) => {
+    if (!draggedImageUrl || !targetImageUrl || draggedImageUrl === targetImageUrl) {
+      return
+    }
+
+    const targetPageIndex =
+      targetImageUrl.includes('page') &&
+      Number(targetImageUrl.slice(targetImageUrl.indexOf('-') + 1))
+
+    setData(
+      data.map((page, index) => {
+        if (targetPageIndex === index && page.images.some(img => img === draggedImageUrl)) {
+          return page
+        }
+
+        if (typeof targetPageIndex === 'number' && targetPageIndex !== index) {
+          return {
+            ...page,
+            images: page.images.filter(img => img !== draggedImageUrl)
+          }
+        }
+
+        if (targetPageIndex === index) {
+          return {
+            ...page,
+            images: [...page.images, draggedImageUrl]
+          }
+        }
+
+        return {
+          ...page,
+          images: page.images.map(img => {
+            if (img === draggedImageUrl) {
+              return targetImageUrl
+            }
+            if (img === targetImageUrl) {
+              return draggedImageUrl
+            }
+            return img
+          })
+        }
+      })
+    )
+  }
+
   return (
     <div>
       <Head>
@@ -36,30 +107,7 @@ export default function Testpage() {
         <h1>Trip to the Beach</h1>
         <p>Hardback Photobook last edited on Thursday 13 April 2022 at 16:28</p>
       </PageHeader>
-      <PrintPage
-        data={[
-          {
-            title: "Front Print",
-            images: [
-              "https://videodelivery.net/775b1b7196b2c126b8dc343416211fdb/thumbnails/thumbnail.jpg?height=1080",
-            ],
-          },
-          {
-            title: "Page 2",
-            images: [
-              "https://videodelivery.net/9ad2bb839e4e3cc1074e5d73b0a0379b/thumbnails/thumbnail.jpg?height=1080",
-              "https://imagedelivery.net/66_qOEcY2UwnECf5ON9PhQ/bde5b129-52ba-4f43-b3f4-97591952ea00/large",
-            ],
-          },
-          {
-            title: "Page 3",
-            images: [
-              "https://videodelivery.net/91097538e177847ebeb934a492e146e9/thumbnails/thumbnail.jpg?height=1080",
-              "https://imagedelivery.net/66_qOEcY2UwnECf5ON9PhQ/b73c2865-7a02-408b-654d-89ce2512ae00/large",
-            ],
-          },
-        ]}
-      />
+      <PrintPage handleDrop={handleDrop} data={data} />
     </div>
-  );
+  )
 }
